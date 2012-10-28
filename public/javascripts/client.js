d6aa2759513665eef4cd2634f5fae39f04774507
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2012-10-28 12:01:58 +0900

    excale実験01

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,6 @@
-//var socket = io.connect('http://niconicoar.excale.net', {transports: ["xhr-polling"]});
+var socket = io.connect('http://niconicoar.excale.net', {transports: ["xhr-polling"]});
 //var socket = io.connect('http://192.168.1.102:8080', {transports: ["xhr-polling"]});
-var socket = io.connect('http://niconicoar.herokuapp.com', {transports: ["xhr-polling"]});
+//var socket = io.connect('http://niconicoar.herokuapp.com', {transports: ["xhr-polling"]});
 //var socket = io.connect('http://localhost:8080', {transports: ["xhr-polling"]});
 
 $(document).ready(function() {
